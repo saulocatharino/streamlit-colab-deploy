d016e0cb7f22de73d47465b8c7a10ef80f7e3dff
Saulo Catharino <saulocatharino@gmail.com>
2021-09-16 19:33:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 filename = 'video.mp4'
-if st.button("Play"):
-    video_file = open(filename, 'rb')
-    video_bytes = video_file.read()
-    st.video(video_bytes)
+video_file = open(filename, 'rb')
+video_bytes = video_file.read()
+st.video(video_bytes)
